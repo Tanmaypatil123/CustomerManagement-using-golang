c57a7bdfdce20187cc8d21ea69891bbfab56d7ad
Tanmaypatil123 <77950208+Tanmaypatil123@users.noreply.github.com>
2022-03-31 22:19:01 +0530

    First commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module CustomerManagement
+
+go 1.17
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+func goDotEnvVariable(key string) string {
+
+	// load .env file
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
+
+type Customer struct {
+	CustomerId int
+	CustomerName string
+	SSN string
+}
+
+func  GetConnection()(database *sql.DB)  {
+	databaseDriver := "mysql"
+	databaseUser := "root"
+	databasePass := goDotEnvVariable("DATABASE_PASS")
+	databaseName := "crm"
+	database , error := sql.Open(databaseDriver,databaseUser+":"+databasePass+"@/"+databaseName)
+	if error != nil {
+		panic(error.Error())
+	}
+	return database
+}
+
+func GetCustomer() []Customer{
+	var database *sql.DB
+	database = GetConnection()
+	var error error
+	var rows *sql.Rows
+	rows , error = database.Query("SELECT  * FROM Customer ORDER BY Customerid DESC ")
+	if error != nil {
+		panic(error.Error())
+	}
+	var customer Customer
+	customer = Customer{}
+
+	var customers []Customer
+	customers =[]Customer{}
+	for rows.Next() {
+		var customerId int
+		var customerName string
+		var ssn string
+		error = rows.Scan(&customerId,&customerName,&ssn)
+		if error != nil {
+			panic(error.Error())
+		}
+		customer.CustomerId = customerId
+		customer.CustomerName = customerName
+		customer.SSN = ssn
+		customers = append(customers,customer)
+	}
+	defer database.Close()
+	return customers
+}
+
+func main() {
+	var customers []Customer
+	customers = GetCustomer()
+	fmt.Println("Customers",customers)
+}
